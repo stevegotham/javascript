2ad3bce459ace80b26350f5045905da7ebf1b170
Steve Gotham <g23design@gmail.com>
2014-05-29 19:22:38 -0600

    refactor to better deal with unexpected answers to user prompts

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -5,60 +5,60 @@ var computer =0;
 
 for (i=0;i<rounds;i++) {
 
-var userChoice = prompt ("Choose rock, paper, or scissors.");
+	var userChoice = prompt ("Choose rock, paper, or scissors.");
 
-if (userChoice!=="rock" && userChoice!=="paper" && userChoice!=="scissors") {
-	console.log("You must choose rock, paper, or scissors");
-	break;
-}
-
-var computerChoice = Math.random();
-
-if (computerChoice < .33) {
-	computerChoice = "rock";
-}
-else if (computerChoice < .66) {
-	computerChoice = "paper";
+	if (userChoice!=="rock" && userChoice!=="paper" && userChoice!=="scissors") {
+		console.log("You must choose rock, paper, or scissors");
+		break;
 	}
-else {
-	computerChoice = "scissors";
-}
 
-function compare(var1,var2) {
-	if (var1 == var2) {
-		 console.log("It's a tie!");
+	var computerChoice = Math.random();
+
+	if (computerChoice < .33) {
+		computerChoice = "rock";
 	}
-	else if (var1 == "rock") {
-		if (var2 == "paper") {
-			console.log("Paper wins!");
-			computer = computer + 1;
+	else if (computerChoice < .66) {
+		computerChoice = "paper";
+	}
+	else {
+		computerChoice = "scissors";
+	}
+
+	function compare(var1,var2) {
+		if (var1 == var2) {
+			 console.log("It's a tie!");
 		}
-		else {
-			console.log("Rock wins!");
-			player = player + 1;
-	}
-}
-	else if (var1 == "paper") {
-		if (var2 == "scissors") {
-			console.log("Scissors wins!");
-			computer = computer + 1;
+		else if (var1 == "rock") {
+			if (var2 == "paper") {
+				console.log("Paper wins!");
+				computer = computer + 1;
+			}
+			else {
+				console.log("Rock wins!");
+				player = player + 1;
+			}
 		}
-		else {
-			console.log("Paper wins!");
-			player = player + 1;
-}
-	}
-	else if (var1 == "scissors") {
-		if (var2 == "rock") {
-			console.log("Rock wins!");
-			computer = computer + 1;
+		else if (var1 == "paper") {
+			if (var2 == "scissors") {
+				console.log("Scissors wins!");
+				computer = computer + 1;
+			}
+			else {
+				console.log("Paper wins!");
+				player = player + 1;
+			}
 		}
-		else {
-			console.log("Scissors wins!");
-			player = player + 1;
+		else if (var1 == "scissors") {
+			if (var2 == "rock") {
+				console.log("Rock wins!");
+				computer = computer + 1;
+			}
+			else {
+				console.log("Scissors wins!");
+				player = player + 1;
+			}
+		}
 	}
-}
-}
 
 	console.log("You chose " + userChoice + ".");
 	console.log("The computer chose " + computerChoice + ".");	
@@ -69,6 +69,9 @@ function compare(var1,var2) {
 
 if (player > computer) {
 	console.log("You won!!");
+}
+else if (player == computer) {
+	console.log("Let's try again.");
 }
 else {
 	console.log("You lost!! :(");
